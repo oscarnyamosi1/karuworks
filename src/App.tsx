89db4633c358931b91c122817816ee47029a65fe
malik taylor l-mint <oscarnyamosi2@gmail.com>
2026-06-22 16:46:20 +0300

    installed vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Analytics } from "@vercel/analytics/next"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import FloatingActions from "./components/FloatingActions";
